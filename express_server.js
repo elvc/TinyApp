8a561669a2a045f77e341b15139a1ef23cc5d2fe
Elvis Chan <choectt@gmail.com>
2017-04-11 23:07:15 +0000

    first commit

---
+++ express_server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.set("view engine", "ejs");
+
+function generateRandomString() {
+
+}
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
+});
+
+// post method
+app.post("/urls", (req, res) => {
+  console.log(req.body); // debug statement to see POST parameters
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+function generateRandomString() {
+  let string = "";
+  const CHAR_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 6; i++)
+    string += CHAR_SET.charAt(Math.floor(Math.random() * CHAR_SET.length));
+
+  return string;
+}
